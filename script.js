37d17cc17b0fa8c742bb2bef27b8e3e00d43496e
Pablo22Tucu <109873520+Pablo22Tucu@users.noreply.github.com>
2024-09-27 12:19:59 -0300

    version 1.0
    Lanzamiento primera version

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggle-mode').addEventListener('click', function() {
-  document.body.classList.toggle('night-mode');
-  if (document.body.classList.contains('night-mode')) {
-    this.textContent = 'Modo Claro';
-  } else {
-    this.textContent = 'Modo Nocturno';
-  }
-});
